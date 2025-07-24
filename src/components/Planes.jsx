d9290040a3a4e71a2aeaceeed169963b34c66741
Kenny Gabriel Perez Rosales <121875673+KennyGi0053@users.noreply.github.com>
2025-07-24 18:02:33 -0400

    componente planes avanzado

--- src/components/Planes.jsx
+++ src/components/Planes.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
-
+import check from "../assets/checkin.png"
 const Planes = () => {
   return (
-    <div>
-      <h1 className='text-white'>Planes y Servicios</h1>
+    <div className='p-4 mt-20 '>
+      <h1 className='text-white text-xl font-bold text-center'>Planes y Servicios</h1>
+    <div className='md:ml-10 mt-10 border-2 border-gray-400 rounded-lg shadow-2xl h-92 bg-[#1f1f26] transform transition-all duration-300 hover:shadow-indigo-500/100 overflow-x-auto'>
+      <div className='ml-6 mt-8 '>
+
+      <h2 className='text-xl font-semibold text-white mt-4'>Básico</h2>
+      <h1 className='text-2xl text-white mt-4'>299$/mes</h1>
+      <div className='text-white mt-4 '  >
+        <div className="flex flex-row items-center mt-6 gap-4 ">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >Gestión de redes sociales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >12 publicaciones mensuales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+
+        <p >Respuesta a comentarios</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >informe mensual básico</p>
+        </div>
+      </div>
+      </div>
+      <button className='mt-8 border-1 border-gray-400 text-white mx-auto block rounded-lg h-10 w-3/4'>
+      Elegir Plan
+      </button>
+    </div>
+    <div className='md:ml-10 mt-10 border-2 border-gray-400 rounded-lg shadow-2xl h-92 bg-[#1f1f26] transform transition-all duration-300 hover:shadow-indigo-500/100 overflow-x-auto'>
+      <div className='ml-6 mt-8 '>
+
+      <h2 className='text-xl font-semibold text-white mt-4'>Básico</h2>
+      <h1 className='text-2xl text-white mt-4'>299$/mes</h1>
+      <div className='text-white mt-4 '  >
+        <div className="flex flex-row items-center mt-6 gap-4 ">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >Gestión de redes sociales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >12 publicaciones mensuales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+
+        <p >Respuesta a comentarios</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >informe mensual básico</p>
+        </div>
+      </div>
+      </div>
+      <button className='mt-8 border-1 border-gray-400 text-white mx-auto block rounded-lg h-10 w-3/4'>
+      Elegir Plan
+      </button>
+    </div>
+    <div className='md:ml-10 mt-10 border-2 border-gray-400 rounded-lg shadow-2xl h-92 bg-[#1f1f26] transform transition-all duration-300 hover:shadow-indigo-500/100 overflow-x-auto'>
+      <div className='ml-6 mt-8 '>
+
+      <h2 className='text-xl font-semibold text-white mt-4'>Básico</h2>
+      <h1 className='text-2xl text-white mt-4'>299$/mes</h1>
+      <div className='text-white mt-4 '  >
+        <div className="flex flex-row items-center mt-6 gap-4 ">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >Gestión de redes sociales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >12 publicaciones mensuales</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+
+        <p >Respuesta a comentarios</p>
+        </div>
+        <div className="flex flex-row items-center mt-2 gap-4">
+        <img src={check} alt="" className="h-4 w-4" />
+        <p >informe mensual básico</p>
+        </div>
+      </div>
+      </div>
+      <button className='mt-8 border-1 border-gray-400 text-white mx-auto block rounded-lg h-10 w-3/4'>
+      Elegir Plan
+      </button>
+    </div>
     </div>
   )
 }
